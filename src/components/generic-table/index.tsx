4fd0a5a5f46353d71a5fb5b702c7167a05b1f829
Jeroen Vervaeke <jeroen@vervaeke.pro>
2019-03-18 14:35:20 +0100

    Fixed issue with explicit any

--- src/components/generic-table/index.tsx
+++ src/components/generic-table/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 
 type ObjectWithReactNodeProps<T> = { [K in keyof T]: React.ReactNode };
-type DisplayMethods<T> = { [K in keyof T]?: (value: T[K]) => React.ReactNode };
+type DisplayMethods<T> = {
+  [K in keyof T]?: (value: React.ReactNode) => React.ReactNode
+};
 type ColumnTitles<T> = { [K in keyof T]: string };
 
 export interface GenericTableProps<
@@ -9,12 +11,24 @@ export interface GenericTableProps<
 > {
   data: TData[];
   displayMethods?: DisplayMethods<TData>;
-  titles: ColumnTitles<TData>;  
+  titles: ColumnTitles<TData>;
+}
+
+// Dirty hack to make getOwnPropertyNames work properly
+declare class Object {
+  static getOwnPropertyNames<T>(o: T): (keyof T)[];
 }
 
 export class GenericTable<
   TData extends ObjectWithReactNodeProps<TData>
 > extends React.PureComponent<GenericTableProps<TData>> {
+  renderCell(
+    cell: React.ReactNode,
+    renderer?: (value: React.ReactNode) => React.ReactNode
+  ) {
+    return renderer !== undefined ? renderer(cell) : cell;
+  }
+
   render() {
     const { data, displayMethods, titles } = this.props;
 
@@ -29,8 +43,8 @@ export class GenericTable<
           <tr key={ri}>
             {Object.getOwnPropertyNames(row).map((colName, ci) => (
               <td key={ci}>
-                {displayMethods[colName] !== undefined
-                  ? displayMethods[colName](row[colName])
+                {displayMethods !== undefined
+                  ? this.renderCell(row[colName], displayMethods[colName])
                   : row[colName]}
               </td>
             ))}
